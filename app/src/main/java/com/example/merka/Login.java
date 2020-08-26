6f70655a959cb627fc41b00a19a366a8edb8d255
nicolasarcas <nicolas.arcas1@gmail.com>
2020-08-26 19:00:22 -0300

    Terminar o método de cadastro/login

--- app/src/main/java/com/example/merka/Login.java
+++ app/src/main/java/com/example/merka/Login.java
@@ -1,13 +1,28 @@
 package com.example.merka;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    private Button btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnEntrar=findViewById(R.id.buttonEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (Login.this, Tela_Inicial.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/merka/Tela_Inicial.java
@@ -0,0 +1,14 @@
+package com.example.merka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela__inicial);
+    }
+}
