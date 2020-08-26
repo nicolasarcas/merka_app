6bad9a36ea13afea52555d57e9000e69451bfd0e
nicolasarcas <nicolas.arcas1@gmail.com>
2020-08-26 18:45:53 -0300

    26/08

--- app/src/main/java/com/example/merka/MainActivity.java
+++ app/src/main/java/com/example/merka/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.merka;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastrar = findViewById(R.id.buttonCriarConta);
+        btnLogin = findViewById(R.id.buttonEntrar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
